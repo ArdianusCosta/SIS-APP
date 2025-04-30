ae159e21e943aa554df9bcb3642fbaf8831962f9
ArdianusCosta <costaaja017@gmail.com>
2025-04-30 14:58:03 +0700

    Untuk akun yang di banned

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class,
-        'checkstatus' => \App\Http\Middleware\CheckStatus::class,
+        'check.status' => \App\Http\Middleware\CheckStatus::class,
     ];
 }
