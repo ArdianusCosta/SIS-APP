ebcbd0b53b8e5d006a6046f099037caec43b5776
ArdianusCosta <costaaja017@gmail.com>
2025-05-20 11:26:36 +0700

    perbaikan routing

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,6 @@ Route::get('auth/forgotPW/forgot-password', [ForgotPasswordController::class, 's
 Route::post('auth/forgotPW/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
 Route::get('auth/forgotPW/reset-password/{token}', [ForgotPasswordController::class, 'showResetForm'])->name('password.reset');
 Route::post('auth/forgotPW/reset-password', [ForgotPasswordController::class, 'reset'])->name('password.update');
-
 //api-login
 Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
 Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
@@ -54,40 +53,7 @@ Route::post('/user/store', [UserController::class, 'store'])->name('user.store')
 Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
 Route::put('/user/update/{id}', [UserController::class, 'update'])->name('user.update');
 Route::delete('/user/hapus/{id}', [UserController::class, 'destroy'])->name('user.hapus');
-});
-
-//Absensi SIS-APP
-Route::middleware(['auth','check.status','role:admin,guru'])->group(function (){
-Route::get('/absensi/input/list', [AbsensiController::class, 'list'])->name('absensi.list');
-Route::get('/absensi/input/create', [AbsensiController::class, 'create'])->name('absensi-input.create');
-Route::post('/absensi/input/create', [AbsensiController::class, 'store'])->name('absensi-input.store');
-Route::get('/absensi/input/cari', [AbsensiController::class, 'cariSiswa']);
-//scan
-Route::get('/absensi/scan/index-scan', [AbsensiController::class, 'indexScan'])->name('index-scan');
-Route::post('/absensi/scan/index-scan', [AbsensiController::class, 'generateQr'])->name('generate-qr.code');
-Route::get('/absensi/scan/scan-kamera', [AbsensiController::class, 'scanKamera'])->name('scan-kamera');
-});
-
-//informasi & penggumuman 
-//Email
-Route::middleware(['auth','check.status','role:admin,guru'])->group(function (){
-Route::get('/email/create', [MessageController::class, 'create'])->name('email.create');
-Route::post('/email/create', [MessageController::class, 'store'])->name('email.store');
-});
-
-//ini downloadnya bisa semua role
-Route::middleware(['auth','check.status','role:admin,guru,murid'])->group(function(){
-Route::get('/download/{filename}', [MessageController::class, 'download'])->name('download.file');
-});
-
-//profile SIS-APP
-Route::middleware(['auth','check.status','role:admin,guru,murid'])->group(function (){
-Route::get('/profile/index', [ProfileController::class, 'index'])->name('profile.index');
-Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
-});
-
-//guru 
-Route::middleware(['auth','check.status','role:admin'])->group(function (){
+//manajement-guru
 Route::get('/manajement/guru/index', [GuruController::class, 'index'])->name('guru.index');
 Route::get('/manajement/guru/create', [GuruController::class, 'create'])->name('guru.create');
 Route::post('/manajement/guru/create', [GuruController::class, 'store'])->name('guru.store');
@@ -95,19 +61,11 @@ Route::get('/manajement/guru/edit/{id}', [GuruController::class, 'edit'])->name(
 Route::post('/manajement/guru/edit/{id}', [GuruController::class, 'update'])->name('guru.update');
 Route::delete('/manajement/guru/hapus/{id}', [GuruController::class, 'destroy'])->name('guru.hapus');
 Route::get('/exportGuru', [GuruController::class, 'export'])->name('guru.export');
-//kontak
-Route::get('/kontak-guru/index', [GuruController::class, 'kontakGuru'])->name('kontak-guru');
-Route::get('/exportGuru', [GuruController::class, 'export'])->name('guru.export');
-//kontak
-Route::get('/kontak-guru/index', [GuruController::class, 'kontakGuru'])->name('kontak-guru');
 });
 
-Route::middleware(['auth','check.status','role:admin,guru,murid'])->group(function (){
-Route::get('/kontak-guru/index', [GuruController::class, 'kontakGuru'])->name('kontak-guru');   
-});
 
-//ortu
-Route::middleware(['auth','check.status','role:admin,guru'])->group(function (){
+Route::middleware(['auth','check.status','role:admin,guru'])->group(function(){
+//manajement-ortu
 Route::get('/manajement/ortu/index', [OrangTuaController::class, 'index'])->name('ortu.index');
 Route::get('/manajement/ortu/create', [OrangTuaController::class, 'create'])->name('ortu.create');
 Route::post('/manajement/ortu/create', [OrangTuaController::class, 'store'])->name('ortu.store');
@@ -116,10 +74,7 @@ Route::post('/manajement/ortu/edit/{id}', [OrangTuaController::class, 'update'])
 Route::delete('/manajement/ortu/destroy/{id}', [OrangTuaController::class, 'destroy'])->name('ortu.hapus');
 Route::get('/exportOrangTua', [OrangTuaController::class,'export'])->name('ortu.export');
 Route::post('/importOrangTua', [OrangTuaController::class, 'import'])->name('ortu.import');
-});
-
-//siswa
-Route::middleware(['auth','check.status','role:admin,guru'])->group(function(){
+//manajement-siswa
 Route::get('/manajement/siswa/index', [SiswaController::class, 'index'])->name('siswa.index');
 Route::get('/manajement/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
 Route::post('/manajement/siswa/create', [SiswaController::class, 'store'])->name('siswa.store');
@@ -128,46 +83,57 @@ Route::post('/manajement/siswa/edit/{id}', [SiswaController::class, 'update'])->
 Route::delete('/manajement/siswa/destroy/{id}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
 Route::post('/importSiswa', [SiswaController::class, 'import'])->name('siswa.import');
 Route::get('/exportSiswa', [SiswaController::class, 'export'])->name('siswa.import');
+//absensi-SIS
+Route::get('/absensi/input/list', [AbsensiController::class, 'list'])->name('absensi.list');
+Route::get('/absensi/input/create', [AbsensiController::class, 'create'])->name('absensi-input.create');
+Route::post('/absensi/input/create', [AbsensiController::class, 'store'])->name('absensi-input.store');
+Route::get('/absensi/input/cari', [AbsensiController::class, 'cariSiswa']);
+//scan
+Route::get('/absensi/scan/index-scan', [AbsensiController::class, 'indexScan'])->name('index-scan');
+Route::post('/absensi/scan/index-scan', [AbsensiController::class, 'generateQr'])->name('generate-qr.code');
+Route::get('/absensi/scan/scan-kamera', [AbsensiController::class, 'scanKamera'])->name('scan-kamera');
+//informasi & penggumuman 
+//Email
+Route::get('/email/create', [MessageController::class, 'create'])->name('email.create');
+Route::post('/email/create', [MessageController::class, 'store'])->name('email.store');
+//khusus
+Route::get('/informasi/create', [InfomasiController::class, 'create'])->name('informasi.create');
+Route::post('/informasi/create', [InfomasiController::class, 'store'])->name('informasi.store');
 });
 
-//Kelas
+
 Route::middleware(['auth','check.status','role:admin,guru,murid'])->group(function(){
+//contact untuk hubugin bang costaaja .. wop tapi belum jadi
+Route::get('/contact_to_speatseed/index-spreatseed', [ContactController::class, 'index'])->name('contact-to-speedseat.index');
+Route::post('/contact_to_speatseed/index-spreatseed', [ContactController::class, 'store'])->name('contact-to-speedseat.store');
+//surat
+Route::get('/surat-izin/index', [SuratController::class, 'index'])->name('surat.index');
+Route::get('/surat-izin/keluar-kelas/create', [SuratController::class, 'createKeluarKelas'])->name('keluar-kelas.create');
+Route::post('/surat-izin/keluar-kelas/create', [SuratController::class, 'storeKeluarKelas'])->name('keluar-kelas.store');
+Route::get('/surat-izin/keluar-kelas/surat/{id}', [SuratController::class, 'showKeluarKelas'])->name('keluar-kelas.surat');
+Route::get('/surat-izin/keluar-sekolah/create', [SuratController::class, 'createKeluarSekolah'])->name('keluar-sekolah.create');
+Route::post('/surat-izin/keluar-sekolah/create', [SuratController::class, 'storeKeluarSekolah'])->name('keluar-sekolah.store');
+Route::get('/surat-izin/keluar-sekolah/surat/{id}', [SuratController::class, 'showKeluarSekolah'])->name('keluar-sekolah.surat');
+Route::get('/surat-izin/kelab/create', [SuratController::class, 'createKeLab'])->name('kelab-create');
+Route::post('/surat-izin/kelab/create', [SuratController::class, 'storeKeLab'])->name('kelab-store');
+Route::get('/surat-izin/kelab/surat/{id}',[SuratController::class, 'showKeLab'])->name('surat-kelab');
+//informasi khusus
+Route::get('/informasi/index', [InfomasiController::class, 'index'])->name('informasi.index');
+//profile SIS-APP
+Route::get('/profile/index', [ProfileController::class, 'index'])->name('profile.index');
+Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+//ini downloadnya bisa semua role
+Route::get('/download/{filename}', [MessageController::class, 'download'])->name('download.file');
+//manajement-akademik
+Route::get('/akademik/index', [AkademikController::class, 'index'])->name('akademik.index');
+//kontak-guru
+Route::get('/kontak-guru/index', [GuruController::class, 'kontakGuru'])->name('kontak-guru'); 
+//manajement-kelas
 Route::get('/manajement/kelas/index', [KelasController::class,'index'])->name('kelas.index');
 Route::get('/manajement/kelas/create', [KelasController::class,'create'])->name('kelas.create');
 Route::post('/manajement/kelas/create', [KelasController::class,'store'])->name('kelas.store');
 Route::get('/manajement/kelas/edit/{id}', [KelasController::class,'edit'])->name('kelas.edit');
 Route::post('/manajement/kelas/edit/{id}', [KelasController::class,'update'])->name('kelas.update');
 Route::delete('/manajement/kelas/destroy/{id}', [KelasController::class,'destroy'])->name('kelas.destroy');
-Route::get('/exportKelas', [KelasController::class,'export'])->name('kelas.export');
+Route::get('/exportKelas', [KelasController::class,'export'])->name('kelas.export');  
 });
-
-//contact untuk hubugin bang costaaja .. wop tapi belum jadi
-Route::get('/contact_to_speatseed/index-spreatseed', [ContactController::class, 'index'])->name('contact-to-speedseat.index');
-Route::post('/contact_to_speatseed/index-spreatseed', [ContactController::class, 'store'])->name('contact-to-speedseat.store');
-
-//surat
-Route::middleware(['auth','check.status','role:admin,guru,murid'])->group(function(){
-Route::get('/surat-izin/index', [SuratController::class, 'index'])->name('surat.index');
-Route::get('/surat-izin/keluar-kelas/create', [SuratController::class, 'createKeluarKelas'])->name('keluar-kelas.create');
-Route::post('/surat-izin/keluar-kelas/create', [SuratController::class, 'storeKeluarKelas'])->name('keluar-kelas.store');
-Route::get('/surat-izin/keluar-kelas/surat/{id}', [SuratController::class, 'showKeluarKelas'])->name('keluar-kelas.surat');
-    
-Route::get('/surat-izin/keluar-sekolah/create', [SuratController::class, 'createKeluarSekolah'])->name('keluar-sekolah.create');
-Route::post('/surat-izin/keluar-sekolah/create', [SuratController::class, 'storeKeluarSekolah'])->name('keluar-sekolah.store');
-Route::get('/surat-izin/keluar-sekolah/surat/{id}', [SuratController::class, 'showKeluarSekolah'])->name('keluar-sekolah.surat');
-
-Route::get('/surat-izin/kelab/create', [SuratController::class, 'createKeLab'])->name('kelab-create');
-Route::post('/surat-izin/kelab/create', [SuratController::class, 'storeKeLab'])->name('kelab-store');
-Route::get('/surat-izin/kelab/surat/{id}',[SuratController::class, 'showKeLab'])->name('surat-kelab');
-});
-
-//informasi khusus
-Route::middleware(['auth','check.status','role:admin,guru,murid'])->group(function(){
-Route::get('/informasi/index', [InfomasiController::class, 'index'])->name('informasi.index');
-});
-Route::middleware(['auth','check.status','role:admin,guru'])->group(function(){
-Route::get('/informasi/create', [InfomasiController::class, 'create'])->name('informasi.create');
-Route::post('/informasi/create', [InfomasiController::class, 'store'])->name('informasi.store');
-});
-
-Route::get('/akademik/index', [AkademikController::class, 'index'])->name('akademik.index');
